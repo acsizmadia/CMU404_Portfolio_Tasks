cc2e8958bddf1725591794bd64f5b1d4a0a59621
Andrew Csizmadia <a.p.csizmadia@newman.ac.uk>
2021-08-14 21:19:25 +0100

    Add files via upload
    This is the template for Portfolio Task 1: Write a Song

---
+++ Template_PT1_Write_a_Song.py
@@ -0,0 +1,32 @@
+# Template for CMU404: Programming for a Computer Scientist
+# Portfolio Task 1: Write a Song
+# Author:
+# Date:
+# Version Control: 1.0.1
+
+def main():
+    verse_One()
+    chorus()
+    verse_Two()
+    chorus()
+
+def verse_One():
+    # This is verse one of the song
+    print()
+    print("This is verse one")
+    print()
+
+def verse_Two():
+    # This is verse two of the song
+    print()
+    print("This is verse two")
+    print()
+
+def chorus():
+    # This is chorus for the song
+    print()
+    print("This is the chorus for the song")
+    print()
+
+
+main()
